ff9ac444639badd150de61641cc60ee2b20cbe2b
willrosslyn <will@rosslyncoffee.com>
2026-05-20 13:17:07 +0100

    repo clean

--- Data Processing Scripts/Project_test.py
+++
@@ -1,5 +0,0 @@
-import pandas as pd
-
-audio_features = pd.read_parquet('../RAW Data/audio_features.parquet')
-print(audio_features.columns.to_list())
-print(max(audio_features["track_popularity"]),min(audio_features["track_popularity"]))
